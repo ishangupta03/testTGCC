1782f0d19335e622d2288d618c5cd1ae36554646
ishangupta03 <igajm003@gmail.com>
2024-05-05 19:15:58 +0530

    faq

--- pages/faq.js
+++ pages/faq.js
@@ -15,18 +15,18 @@ function Faq() {
       </Head>
       <section className="flex flex-col items-center md:mb-10">
       
-          <h1 className="text-primary_green-0 font-sans text-3xl md:text-5xl text center font-extrabold flex wrap justify-center flex-col md:flex-row mb-10">
+          <h1 className="text-primary_green-0 font-sans text-3xl md:text-5xl text-center font-extrabold mb-5">
             Frequently Asked Questions (FAQs)
           </h1>
         
-        <div className="flex justify-around md:gap-1 w-11/12">
-          <div className="w-6/12">
+        <div className="md:gap-1 w-11/12 flex wrap justify-center flex-col md:flex-row mb-10">
+          <div className="md:w-6/12">
             {data.slice(0, Math.ceil(data.length / 2)).map((curElem) => {
               const { id } = curElem;
               return <DiabloAccordion key={id} {...curElem} />;
             })}
           </div>
-          <div className="w-6/12">
+          <div className="md:w-6/12">
             {data
               .slice(Math.ceil(data.length / 2), data.length)
               .map((curElem) => {
