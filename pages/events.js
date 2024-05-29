ddc86a53ef80d3b6e09df00908cb6783d3f1d04f
ishangupta03 <igajm003@gmail.com>
2024-05-30 03:10:34 +0530

    events

--- pages/events.js
+++ pages/events.js
@@ -13,7 +13,7 @@ function info() {
           href="./TG_Logo_White.png"
         />
       </Head>
-      <div className="container transition-colors mx-auto mt-12 mb-0 md:mb-12 p-8 sm:px-10 md:px-12 lg:px-40 2xl:px-50">
+      <div className="container transition-colors mx-auto mt-8 mb-0 md:mb-12 p-8 sm:px-10 md:px-12 lg:px-40 2xl:px-50">
         <div className="items-center justify-center">
           <div className="text-center text-2xl">
             <p className="text-[#2B4257] text-4xl center font_heading font-bold mb-10">
@@ -22,13 +22,16 @@ function info() {
           </div>
         </div>
 
-            <div className="grid md:grid-cols-3 grid-cols-1 gap-4 md:gap-16" >
-              <div className=""> <InstagramEmbed url="https://www.instagram.com/p/C3-wyKHMIAe" width={328}/></div>
-              <div> <InstagramEmbed url="https://www.instagram.com/p/C2sYJALhxTw" width={328}/></div>
-              <div> <InstagramEmbed url="https://www.instagram.com/p/C2koaUKIWql" width={328}/></div>
-              <div> <InstagramEmbed url="https://www.instagram.com/p/C1MggLiPP9-" width={328}/></div>
-              <div> <InstagramEmbed url="https://www.instagram.com/p/C1B6HxIPR-9" width={328}/></div>
+          
+            <div className="grid md:grid-cols-3 grid-cols-1 md:gap-16 gap-4 justify-items-center">
+              <InstagramEmbed url="https://www.instagram.com/p/C3-wyKHMIAe" width={328} className="mx-auto"/>
+              <InstagramEmbed url="https://www.instagram.com/p/C2sYJALhxTw" width={328} className="mx-auto"/>
+              <InstagramEmbed url="https://www.instagram.com/p/C2koaUKIWql" width={328} className="mx-auto"/>
+              <InstagramEmbed url="https://www.instagram.com/p/C1MggLiPP9-" width={328} className="mx-auto"/>
+              <InstagramEmbed url="https://www.instagram.com/p/C1B6HxIPR-9" width={328} className="mx-auto"/>
             </div>
+
+
           </div>
        
         </div>
