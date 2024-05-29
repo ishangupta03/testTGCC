26ed0a71ada1ce267d735dff568e0f0dfd506eac
ishangupta03 <igajm003@gmail.com>
2024-05-30 02:38:08 +0530

    fix

--- pages/events.js
+++ pages/events.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { InstagramEmbed } from 'react-social-media-embed';
 function info() {
   return (
     <div>
@@ -22,40 +23,13 @@ function info() {
         </div>
         <div className="first-section mb-10 flex flex-col md:flex-row justify-between flex-auto items-center flex-wrap mb-24">
           <div className="basis-full mt-4 md:basis-6/12 md:order-last lg:basis-1/2 lg:order-last relative">
-            <div className="text-black font-medium text-lg 2.25rem 3rem mb-5 font_para">
-              <ul className="list-disc">
-                <li>
-                <strong className="text-blue-500 text-2xl">Project Admins</strong> own the projects where the contributors are
-                  free to contribute in the form of documentation, bug fixes or
-                  adding new features.
-                </li>
-                <li>
-                  PAs have to routinely connect with mentors to discuss about
-                  the progress, proceedings &amp; future vision of the project.
-                </li>
-                <li>
-                  PAs are responsible for final reviews, merging PRs, assigning
-                  labels, guiding the mentors in the review process &amp;
-                  resolving doubts in server.
-                </li>
-              </ul>
-            </div>
+            
 
-            <div className="text-black font-medium text-lg 2.25rem 3rem mb-5 font-sans">
-              <ul className="list-disc">
-                <li>
-                <strong className="text-green-500 text-2xl">Mentors</strong> review the Pull Requests, check for plagiarism, and
-                  suggest changes based on the project&apos;s Contribution
-                  guidelines.
-                </li>
-                <li>
-                  They solve queries, debug issues faced by contributors &amp;
-                  are responsible for guiding contributors actively throughout
-                  the program.{" "}
-                </li>
-              </ul>
+            <div className="flex justify-center" >
+              <div className="mr-2 ml-2"> <InstagramEmbed url="https://www.instagram.com/p/C3-wyKHMIAe/" width={328}/></div>
+              <div className="mr-2 ml-2"> <InstagramEmbed url="https://www.instagram.com/p/C3-wyKHMIAe/" width={328}/></div>
+              <div className="mr-2 ml-2"> <InstagramEmbed url="https://www.instagram.com/p/C3-wyKHMIAe/" width={328}/></div>
             </div>
-
           </div>
         </div>
       </div>
